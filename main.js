ea02530bf1afceed7675661f1986b9e50607bb89
Andres Arevalo <andresfelipear@gmail.com>
2021-10-06 14:32:30 -0700

    continue working in the cart

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
+//Alert when the user click add element to the cart
+
 var listItems = document.querySelectorAll(".breakfast a");
 for(i=0;i<listItems.length;i++){
     listItems[i].addEventListener("click",()=>{
         alert("You added one item to the cart. Check your cart.")
 
     })
+}
+
+//Element total
+var total = document.getElementById("total-value");
+var totalValue = 0.0;
+total.innerText = `$${totalValue.toPrecision(2)}`;
+
+function addItem(number){
+    var element = document.createElement('div');
+    element.setAttribute("id",number);
+    element.innerHTML = ""
+
 }

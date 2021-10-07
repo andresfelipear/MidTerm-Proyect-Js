d16990d149e978aaca4482b9b55cfb0ac6aee272
Andres Arevalo <andresfelipear@gmail.com>
2021-10-06 17:24:27 -0700

    show and hidden shopping-cart

--- main.js
+++ main.js
@@ -14,15 +14,17 @@ for (i = 0; i < listLinks.length; i++) {
 }
 
 //Element total
+var nf = Intl.NumberFormat('de-DE');
 var total = document.getElementById("total-value");
 var totalValue = 0.0;
-total.innerText = `$${totalValue.toPrecision(2)}`;
-
+total.innerText = `$${nf.format(totalValue)} COP`;
 
 function addItem(child) {
     var srcImage = child.children[0].src;
     var title = child.children[1].innerText;
     var value = child.children[2].innerText;
+    var stringValue = value.match(/(\d+.\d+)/)[0].replace(".","");
+    var numValue = parseInt(stringValue);
     var element = document.getElementById('items');
     var item = document.createElement("div");
     item.setAttribute('class', 'item');
@@ -32,7 +34,26 @@ function addItem(child) {
         `<h5 class="value no-margin">${value}</h5>` +
         '</div>' +
         '<svg class="trash-can" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="none" viewBox="0 0 24 24"><path fill="white" d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v10zM9 9h6c.55 0 1 .45 1 1v8c0 .55-.45 1-1 1H9c-.55 0-1-.45-1-1v-8c0-.55.45-1 1-1zm6.5-5l-.71-.71c-.18-.18-.44-.29-.7-.29H9.91c-.26 0-.52.11-.7.29L8.5 4H6c-.55 0-1 .45-1 1s.45 1 1 1h12c.55 0 1-.45 1-1s-.45-1-1-1h-2.5z"></path></svg>';
-
     element.appendChild(item);
+    totalValue+=numValue;
+    total.innerText = `$${nf.format(totalValue)} COP`;
+
 
 }
+
+
+//Show and hidde cart button
+var cart = document.getElementById('button-cart');
+var statusCart = false;
+cart.addEventListener("click",()=>{
+    var displayCart = document.getElementById('cart');
+    if(statusCart){
+        displayCart.style.display = "none";
+        statusCart = false;
+    }
+    else{
+        displayCart.style.display = "initial";
+        statusCart = true;
+    }
+    
+})
